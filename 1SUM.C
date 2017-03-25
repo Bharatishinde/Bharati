e47806c9288e72ab2774da2ef35848f2ff37d4b8
Bharatishinde <bharatimshinde15@gmail.com>
2017-03-25 14:07:28 +0530

    Add files via upload

---
+++ 1SUM.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+int sum=0;
+clrscr();
+printf("\n Enter Two No:");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("\n Sum of No is:%d",sum);
+getch();
+}
