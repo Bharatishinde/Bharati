153b0f2290c3b9e47a3a7b7ba1a698c50f7e9d4d
Bharatishinde <bharatimshinde15@gmail.com>
2017-03-25 16:02:28 +0530

    Add files via upload

---
+++ VOWEL.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char ch;
+	clrscr();
+	printf("Enter any character:");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+	case 'a':
+	printf("Alphabate is Vowel");
+	break;
+	case 'e':
+	printf("Alphabate is Vowel");
+	break;
+	case 'i':
+	printf("Alphabate is Vowel");
+	break;
+	case 'o':
+	printf("Alphabate is Vowel");
+	break;
+	case 'u':
+	printf("Alphabate is Vowel");
+	break;
+      default:
+      printf("Alphabate is Consonent");
+	}
+getch();
+
+}
+
+
+
