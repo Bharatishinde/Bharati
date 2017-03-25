37a1fdad0824ec56cc8dd87b5d6718a9c654dd3d
Bharatishinde <bharatimshinde15@gmail.com>
2017-03-25 14:08:21 +0530

    Add files via upload

---
+++ 4PEREMET.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int len,bre;
+int perimeter;
+clrscr();
+printf("\n Enter Length and breath of rectangle");
+scanf("%d%d",&len,&bre);
+perimeter=2*(len+bre);
+printf("\n Peremeter Of rectangle is:%d");
+getch();
+}
