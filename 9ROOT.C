b461f98679227f23f744fe3c505496c299d54050
Bharatishinde <bharatimshinde15@gmail.com>
2017-03-25 14:26:32 +0530

    Add files via upload

---
+++ 9ROOT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int x,root;
+clrscr();
+printf("\n Enter any No:");
+scanf("%d",&x);
+root=sqrt(x);
+printf("\n Squreroot of No is:%d",root);
+getch();
+}
